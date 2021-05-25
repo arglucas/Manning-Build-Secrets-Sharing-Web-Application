a2dfc26a7c1ab20d5b99efbd20e9ed1fa8386a2d
Alex Lucas <arglucas@hotmail.com>
2021-05-25 22:25:00 +0100

    Refactored handlers into handlers module.

--- secret-app/go.mod
+++ secret-app/go.mod
@@ -1,3 +1,11 @@
 module github.com/arglucas/secret-app
 
 go 1.16
+
+require (
+	"github.com/arglucas/secret-app/handlers" v0.0.0
+)
+
+replace (
+	github.com/arglucas/secret-app/handlers => ./handlers
+)
--- secret-app/main.go
+++ secret-app/main.go
@@ -1,44 +1,17 @@
 package main
 
 import (
+	"github.com/arglucas/secret-app/handlers"
 	"log"
 	"net/http"
-	"sync"
-	"time"
 )
 
-// DataStore Data Store for the storing of a mapping from ID to a SecretResponse.
-var DataStore map[string]SecretResponse
-var mutex *sync.Mutex
 
-type SecretRequest struct{
-	PlainText string `json:"plain_text"`
-}
-
-type SecretResponse struct{
-	Id string `json:"id,omitempty"`
-	Data string `json:"data,omitempty"`
-}
-
-// Middleware HTTP Handler, that logs the Request.
-func logger(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		t1 := time.Now()
-		next.ServeHTTP(w, r)
-		t2 := time.Now()
-		log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
-	}
-
-	return http.HandlerFunc(fn)
-}
 
 // Storage and Global initialization.
 // Called on package initialisation (before main etc).
 func init() {
-	// Initialize the data store
-	DataStore = make(map[string]SecretResponse)
-	mutex = &sync.Mutex{}
-	initHandlers()
+	//handlers.InitHandlers()
 }
 
 // Start an HTTP Server on :8080 and hook the SecretHandler to it.
@@ -46,12 +19,12 @@ func main() {
 	addr := ":8080"
 
 	//index := IndexHandler{}
-	handler := SecretHandler{}
-	healthHandler := HealthCheckHandler{}
+	handler := handlers.SecretHandler{}
+	healthHandler := handlers.HealthCheckHandler{}
 
 	//http.HandleFunc("/", Index)
-	http.Handle("/healthcheck", logger(healthHandler))
-	http.Handle("/", logger(handler))
+	http.Handle("/healthcheck", handlers.Logger(healthHandler))
+	http.Handle("/", handlers.Logger(handler))
 
 	log.Printf("Starting on %s", addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
